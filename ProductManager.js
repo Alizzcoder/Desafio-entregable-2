8ef1c0a953c7548d7ab1a763ae62a941aa049949
Aliza Gero <alizacoder@gmail.com>
2023-06-19 20:20:49 -0300

    cambios

--- ProductManager.js
+++ ProductManager.js
@@ -1,11 +1,9 @@
-const fs = require ('fs');
+const fs = require ("fs");
 
 class ProductManager {
     constructor(path) {
         this.path = path;
-       
-    }
-
+        }
 
 async getProducts(){
     try {
@@ -14,50 +12,50 @@ async getProducts(){
             const productsjs = JSON.parse(products);
             return productsjs;
         } else {
-            return []
+            return [];
         }
     } catch (error) {
         console.log(error);
     }
-
     }
 
- async addProduct (title, decription, price, thumbnail, code, stock ) {
-    try {
-        const productsFile = await this.getProducts();
-        if (!title || !decription ||  !price || !thumbnail  || !code || !stock){
-        console.log ('los campos son obligatorios')
-         return;
-         }
-
-        if (this.productsFile.some (product => product.code === code )) {
-        console.log ('Code ya existe')
-         return;
-         }
- 
-         const product = {
-             title,
-             decription,
-             price,
-             thumbnail,
-             code,
-             stock,
-             id: this.#getMaxId() + 1
-         };
-         
-         productsFile.push(product);
-         await fs.promises.writeFile(this.path, JSON.stringify(productsFile))
-         
+    async addProduct (title, decription, price, thumbnail, code, stock ) {
+        try {
+            const productsFile = await this.getProducts();
+            if (!title || !decription ||  !price || !thumbnail  || !code || !stock){
+            console.log ('los campos son obligatorios')
+             return;
+             }
+    
+            if (productsFile.some (product => product.code === code )) {
+            console.log ('Code ya existe')
+             return;
+             }
+     
+             const product = {
+                 title,
+                 decription,
+                 price,
+                 thumbnail,
+                 code,
+                 stock,
+                 id: await this.#getMaxId() + 1
+             };
+             
+             productsFile.push(product);
+             await fs.promises.writeFile(this.path, JSON.stringify(productsFile))
+             
+            }
+        catch (error) {
+            console.log (error);
         }
-    catch (error) {
-        console.log (error);
+       
     }
-   
-}
 
-#getMaxId(){
+async #getMaxId(){
     let maxId = 0;
-    this.productsFile.map((product) =>{
+    const productsFile = await this.getProducts();
+    productsFile.map((product) =>{
         if(product.id > maxId) maxId = product.id;
     })
     return maxId;
@@ -74,30 +72,54 @@ async getProductById(id){
         console.log ( error)
     }
 }
-async updateProduct (id, ...campos){
+async updateProduct (obj, id){
     try{
-        const delProduct = await this.getProducts();
-        const posicionId = await delProduct.indexOf (id);
-// con la posicion del id busco el elemento
-// Buscar como agregar los campos sino se cuales son y cuantos son?
+        const productsFile = await this.getProducts();
+        // busco el producto con la posición del id
+        const posicionId = productsFile.findIndex(product => product.id === id)
+        if (posicionId === -1){
+            console.log ( 'id no encontado');
+        }else {
+            productsFile[posicionId] = {...obj,id } //  agrego el objeto al array en la posicion del id encontrado
+        }
+        await fs.promises.writeFile (this.path, JSON.stringify(productsFile));
     }
     catch (error) {
         console.log (error)
     }
-
 }
 
 async deleteProduct (id){
     try{
         const delProduct = await this.getProducts();
-        const posicionId = await delProduct.indexOf (id);
-        const productFileBorrado = delProduct.splice (posicionId, 1);
-        await fs.promises.writeFile(this.path, JSON.stringify(productFileBorrado))
+        const posicionId = productsFile.findIndex(product => product.id === id)
+        if (posicionId === -1){
+            console.log (' ID de producto no encontado');
+        } else {
+            const productFileBorrado = delProduct.splice (posicionId, 1);
+            await fs.promises.writeFile(this.path, JSON.stringify(productFileBorrado))
+        }
+        
     }
     catch (error) {
         console.log (error)
     }
-
+}
 }
 
-}
+/*--------------------------TEST-----------------------*/
+const productManager = new ProductManager('./productos.json');
+console.log (productManager.getProducts());
+
+productManager.addProduct('producto prueba','Este es un producto prueba',200,'Sin imagen','abc123', 25 )
+console.log (productManager.getProducts());
+
+productManager.addProduct('producto prueba','Este es un producto prueba',200,'Sin imagen','abc126', 25 )
+console.log (productManager.getProducts());
+
+//console.log (productManager.getProductById(2))
+
+console.log ( productManager.updateProduct ({price:1000},1))
+
+//console.log (productManager.deleteProduct(3)); 
+//console.log(await productManager.deleteProduct(1)); 
